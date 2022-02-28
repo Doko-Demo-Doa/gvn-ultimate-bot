8c17fbd587a9c5fb220ac70fa431451e9dddd933
Quan Pham <doraemonfanclub@gmail.com>
2022-02-28 11:02:03 +0700

    Add dotenv

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- main.go
+++ main.go
@@ -1,46 +1,17 @@
 package main
 
 import (
-	"net/http"
+	"doko/gin-sample/app"
+	"log"
 
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-type DownloadEntity struct {
-	id   string `json: "id"`
-	link string `json: "link"`
-	host string `json: "host"`
-}
-
-var sampleLinks = []DownloadEntity{
-	{id: "1", link: "https://google.com", host: "rapidgator"},
-}
-
 func main() {
-	router := gin.Default()
-	router.GET("/downloads", getLinkById)
-	router.POST("/albums", postLink)
-
-	router.Run("localhost:8080")
-}
-
-func getLinks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, sampleLinks)
-}
-
-func postLink(c *gin.Context) {
-	var newLink DownloadEntity
-
-	// Bind the JSON to newLink
-	if err := c.BindJSON(&newLink); err != nil {
-		return
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-	sampleLinks = append(sampleLinks, newLink)
-	c.IndentedJSON(http.StatusCreated, newLink)
-}
-
-func getLinkById(c *gin.Context) {
-	// id := c.Param("id")
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "test message"})
+	app.Run()
 }
