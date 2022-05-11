cf8e983e7c8e1d4578910b4996f94f629289e5d8
Quan Pham <doraemonfanclub@gmail.com>
2022-05-11 07:37:25 +0700

    Add grant role module

---
+++ bot/discord_role_grant_module.go
@@ -0,0 +1,12 @@
+package bot
+
+import "github.com/bwmarrin/discordgo"
+
+var ()
+
+var command = []*discordgo.ApplicationCommand{
+	{
+		Name:        "grant-role",
+		Description: "Grant role command",
+	},
+}
