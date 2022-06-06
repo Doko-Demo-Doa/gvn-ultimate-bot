6aef9aa6ff91cf489214329f434b56a8afb535e2
Quan Pham <doraemonfanclub@gmail.com>
2022-06-07 01:28:50 +0700

    Minor fix

--- repositories/drolerepo/drolerepo.go
+++ repositories/drolerepo/drolerepo.go
@@ -4,6 +4,7 @@ import "doko/gin-sample/models"
 
 type Repo interface {
 	GetByID(id uint) (*models.DiscordRole, error)
-	GetByNativeID(nativeID string) (*models.DiscordRole, error)
-	Assign(user models.DiscordUser) (*models.DiscordUserRole, error)
+	GetByNativeID(nativeId string) (*models.DiscordRole, error)
+	Assign(user models.DiscordUser, toRole models.DiscordRole) (*models.DiscordUserRole, error)
+	Unassign(user models.DiscordUser, fromRole models.DiscordRole) (*models.DiscordUserRole, error) // For history
 }
