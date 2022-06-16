a39d376edf1cc44408a49576c5e3098f03a62e4c
Quan Pham <doraemonfanclub@gmail.com>
2022-06-16 23:47:37 +0700

    Add ban command

--- bot/discord_role_grant_module.go
+++ bot/discord_role_grant_module.go
@@ -17,6 +17,17 @@ func RegisterGrantRoleModule(s *state.State) {
 	)
 
 	s.AddHandler(func(e *gateway.InteractionCreateEvent) {
+		// e.InteractionEvent.Data.InteractionType() = 2 => Command interaction
+		log.Println("222", e.InteractionEvent.Message)
+
+		switch data := e.InteractionEvent.Data.(type) {
+		case *discord.CommandInteraction:
+			log.Println("", data.Name)
+
+		default:
+			log.Println("22")
+		}
+
 		data := api.InteractionResponse{
 			Type: api.MessageInteractionWithSource,
 			Data: &api.InteractionResponseData{
@@ -31,8 +42,54 @@ func RegisterGrantRoleModule(s *state.State) {
 
 	newCommands := []api.CreateCommandData{
 		{
-			Name:        "tiaraping",
-			Description: "Tiaramisu ping",
+			Name:        "vkbp",
+			Description: "Bạn kiếp bất phục",
+			Options: discord.CommandOptions{
+				&discord.UserOption{
+					OptionName:  "target",
+					Description: "Chém, cho thành Covid",
+					Required:    true,
+				},
+				&discord.NumberOption{
+					OptionName:  "ban-duration",
+					Description: "Number of days to covid",
+					Required:    true,
+					Choices: []discord.NumberChoice{
+						{
+							Name:  "1 ngày",
+							Value: 1,
+						},
+						{
+							Name:  "2 ngày",
+							Value: 2,
+						},
+						{
+							Name:  "3 ngày",
+							Value: 3,
+						},
+						{
+							Name:  "4 ngày",
+							Value: 4,
+						},
+						{
+							Name:  "5 ngày",
+							Value: 5,
+						},
+						{
+							Name:  "6 ngày",
+							Value: 6,
+						},
+						{
+							Name:  "7 ngày",
+							Value: 7,
+						},
+					},
+				},
+				&discord.StringOption{
+					OptionName:  "reason",
+					Description: "Lý do covid",
+				},
+			},
 		},
 	}
 
