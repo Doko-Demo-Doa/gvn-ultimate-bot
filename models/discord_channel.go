877a179e3c84e12ad7024bdbf50fe6b00d5acae3
Quan Pham <doraemonfanclub@gmail.com>
2022-06-21 00:02:49 +0700

    Move schema files

--- models/discord_channel.go
+++ models/discord_channel.go
@@ -5,11 +5,11 @@ import "gorm.io/gorm"
 type DiscordChannel struct {
 	gorm.Model
 	NativeId    string `gorm:"NOT NULL;size:255"`
-	ChannelType uint
+	ChannelType uint   `gorm:"DEFAULT:0"` // https://discord.com/developers/docs/resources/channel#channel-object-channel-types
 	Name        string `gorm:"NOT NULL;size:128"`
 	IsNsfw      uint   `gorm:"NOT NULL;DEFAULT:0"`
 }
 
 func (DiscordChannel) TableName() string {
-	return "discord_user"
+	return "discord_channel"
 }
