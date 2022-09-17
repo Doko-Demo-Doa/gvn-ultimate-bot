7baa73efd76fb7b011437891612290567f7872ad
Quan Pham <doraemonfanclub@gmail.com>
2022-09-17 22:00:58 +0700

    Add usercontext middleware

---
+++ middlewares/set_user_context.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/dgrijalva/jwt-go.v3"
+)
+
+// Set the "user" context if the user has valid token, otherwise do nothing
+func SetUserContext(jwtSecret string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token, _ := stripBearer(ctx.Request.Header.Get("Authorization"))
+
+		tokenClaims, _ := jwt.ParseWithClaims(token, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+			return []byte(jwtSecret), nil
+		})
+
+		if tokenClaims != nil {
+			claims, ok := tokenClaims.Claims.(*Claims)
+			if ok && tokenClaims.Valid {
+				// Set gin context values
+			}
+		}
+	}
+}
