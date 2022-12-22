692291dc12c072bd6632b58a50f9580a657f92be
Quan Pham <doraemonfanclub@gmail.com>
2022-12-23 01:55:08 +0700

    Complete role table

--- models/discord_role.go
+++ models/discord_role.go
@@ -1,11 +1,14 @@
 package models
 
+import "time"
+
 type DiscordRole struct {
 	NativeId    string `gorm:"NOT NULL;size:255"`
 	Name        string
 	Mentionable uint // Boolean
 	Hoist       uint // Boolean
 	Color       uint // Color mapped from discord API
+	Expiry      time.Time
 }
 
 func (DiscordRole) TableName() string {
