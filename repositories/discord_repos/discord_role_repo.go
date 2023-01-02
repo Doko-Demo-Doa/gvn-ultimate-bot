c63faa207b65f2c6799b234014528f837bec036e
Quan Pham <doraemonfanclub@gmail.com>
2023-01-02 15:47:50 +0700

    Unassign role

--- repositories/discord_repos/discord_role_repo.go
+++ repositories/discord_repos/discord_role_repo.go
@@ -1,10 +1,61 @@
 package discordrepos
 
-import "doko/gvn-ultimate-bot/models"
+import (
+	"doko/gvn-ultimate-bot/models"
+
+	"gorm.io/gorm"
+)
 
 type DiscordRoleRepo interface {
 	GetByID(id uint) (*models.DiscordRole, error)
 	GetByNativeID(nativeId string) (*models.DiscordRole, error)
 	AssignRole(user models.DiscordUser, toRole models.DiscordRole) (*models.DiscordUserRole, error)
 	UnassignRole(user models.DiscordUser, fromRole models.DiscordRole) (*models.DiscordUserRole, error) // For history
+	CreateRole(role *models.DiscordRole) (*models.DiscordRole, error)                                   // Actually upsert
+}
+
+type discordRoleRepo struct {
+	db *gorm.DB
+}
+
+func NewDiscordRoleRepo(db *gorm.DB) DiscordRoleRepo {
+	return &discordRoleRepo{
+		db: db,
+	}
+}
+
+// AssignRole implements DiscordRoleRepo
+func (*discordRoleRepo) AssignRole(user models.DiscordUser, toRole models.DiscordRole) (*models.DiscordUserRole, error) {
+	panic("unimplemented")
+}
+
+// UnassignRole implements DiscordRoleRepo
+func (*discordRoleRepo) UnassignRole(user models.DiscordUser, fromRole models.DiscordRole) (*models.DiscordUserRole, error) {
+	panic("unimplemented")
+}
+
+func (dr *discordRoleRepo) CreateRole(role *models.DiscordRole) (*models.DiscordRole, error) {
+	var r models.DiscordRole
+	if err := dr.db.Where(&models.DiscordRole{NativeId: role.NativeId}).First(&r).Error; err != nil {
+		return &r, dr.db.Create(r).Error
+	}
+
+	return &r, nil
+}
+
+func (dr *discordRoleRepo) GetByID(id uint) (*models.DiscordRole, error) {
+	var r models.DiscordRole
+	if err := dr.db.First(&r, id).Error; err != nil {
+		return nil, err
+	}
+	return &r, nil
+}
+
+func (dr *discordRoleRepo) GetByNativeID(nativeId string) (*models.DiscordRole, error) {
+	var r models.DiscordRole
+	if err := dr.db.Where(&models.DiscordRole{NativeId: nativeId}).First(&r).Error; err != nil {
+		return nil, err
+	}
+
+	return &r, nil
 }
