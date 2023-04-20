20c46383dff023074966678bb1cc7d55b3354596
Quan Pham <doraemonfanclub@gmail.com>
2023-04-21 00:32:02 +0700

    Change env

--- configs/postgres.go
+++ configs/postgres.go
@@ -10,11 +10,11 @@ import (
 )
 
 type PostgresConfig struct {
-	Host     string `env:"DB_HOST"`
-	Port     int    `env:"DB_PORT"`
-	User     string `env:"DB_USER"`
-	Password string `env:"DB_PASSWORD"`
-	Name     string `env:"DB_NAME"`
+	Host     string `env:"PGHOST"`
+	Port     int    `env:"PGPORT"`
+	User     string `env:"PGUSER"`
+	Password string `env:"PGPASSWORD"`
+	Name     string `env:"PGDATABASE"`
 }
 
 // Dialects returns "postgres"
