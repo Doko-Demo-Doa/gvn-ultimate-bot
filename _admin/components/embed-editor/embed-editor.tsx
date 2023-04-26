151da29f4d7dbcaa72c7df1dfed80531a8529770
Quan Pham <doraemonfanclub@gmail.com>
2023-04-26 08:49:15 +0700

    Minor fix

--- _admin/components/embed-editor/embed-editor.tsx
+++ _admin/components/embed-editor/embed-editor.tsx
@@ -1,23 +1,69 @@
 // Note: This is a pretty complicated component
 
-import { Box, Button, ColorPicker, Popover, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Box,
+  Button,
+  ColorPicker,
+  Group,
+  Paper,
+  Popover,
+  Stack,
+  Text,
+  TextInput,
+  Tooltip,
+  createStyles,
+} from "@mantine/core";
+import { IconSun } from "@tabler/icons-react";
 
 interface Props {
   messageId: string;
 }
 
+const useStyles = createStyles((theme) => ({
+  wrapperFrame: {
+    width: "400px",
+    height: "400px",
+    backgroundColor: "white",
+    display: "flex",
+    justifyContent: "flex-end",
+    borderRadius: 4,
+  },
+  mainFrame: {
+    backgroundColor: "red",
+    width: "calc(100% - 4px)",
+    height: "100%",
+  },
+}));
+
 const EmbedEditor: React.FC<Props> = () => {
+  const { classes } = useStyles();
+
   return (
-    <Box>
-      <Popover closeOnClickOutside position="bottom" withArrow shadow="md">
-        <Popover.Target>
-          <Button compact>Toggle popover</Button>
-        </Popover.Target>
-        <Popover.Dropdown>
-          <ColorPicker format="hex" />
-        </Popover.Dropdown>
-      </Popover>
-    </Box>
+    <Group>
+      <Stack sx={{ maxWidth: "2rem" }}>
+        <Popover closeOnClickOutside position="bottom" withArrow shadow="md">
+          <Popover.Target>
+            <ActionIcon color="red.6" variant="outline">
+              <IconSun size="1rem" />
+            </ActionIcon>
+          </Popover.Target>
+          <Popover.Dropdown>
+            <ColorPicker format="hex" />
+          </Popover.Dropdown>
+        </Popover>
+      </Stack>
+
+      <Stack sx={{ flexGrow: 2 }}>
+        <TextInput placeholder="Write your message here!" />
+
+        <Paper>
+          <Box className={classes.wrapperFrame}>
+            <Box className={classes.mainFrame} />
+          </Box>
+        </Paper>
+      </Stack>
+    </Group>
   );
 };
 
