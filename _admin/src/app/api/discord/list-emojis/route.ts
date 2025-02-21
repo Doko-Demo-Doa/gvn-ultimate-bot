58004fc495bf5b74e366abda968c94a0dd8b7f8a
Quan Pham <doraemonfanclub@gmail.com>
2025-02-21 15:59:03 +0700

    remove discord sdk

--- _admin/src/app/api/discord/list-emojis/route.ts
+++ _admin/src/app/api/discord/list-emojis/route.ts
@@ -1,12 +1,3 @@
-import { Client, GatewayIntentBits } from "discord.js";
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 export async function GET(request: Request) {
-  const oauthGuild = await client.guilds.fetch(
-    process.env.DISCORD_GUILD_ID || "",
-  );
-  const guild = await oauthGuild.fetch();
-  const emojis = await guild.emojis.fetch();
-  const data = emojis.toJSON();
-  return Response.json(data);
+  return Response.json({});
 }
