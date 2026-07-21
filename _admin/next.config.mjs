3157b2fc438a92e13bb34774c4d90edbf631fa1a
Quan Pham <doraemonfanclub@gmail.com>
2026-07-22 01:07:48 +0700

    chore: update dependencies and devDependencies in package.json; add clerk middleware for authentication; modify tsconfig for module resolution and JSX settings

--- _admin/next.config.mjs
+++ _admin/next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  turbopack: {},
 };
 
 export default withVanillaExtract(nextConfig);
