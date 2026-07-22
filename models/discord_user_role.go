bcb8391554e37d5cbdfe760e2ad6c7c181f8881d
Quan Pham <doraemonfanclub@gmail.com>
2026-07-22 13:03:45 +0700

    fix: update DiscordUserRole struct to clarify association handling in queries

--- models/discord_user_role.go
+++ models/discord_user_role.go
@@ -8,10 +8,10 @@ import (
 
 type DiscordUserRole struct {
 	gorm.Model
-	DiscordUser    DiscordUser
-	GrantedRole    DiscordRole
-	UserNativeID   string    `gorm:"size:255;index"`
-	RoleNativeID   string    `gorm:"size:255;index"`
+	DiscordUser    DiscordUser `gorm:"-"` // association ignored in queries; use UserNativeID
+	GrantedRole    DiscordRole `gorm:"-"` // association ignored in queries; use RoleNativeID
+	UserNativeID   string      `gorm:"size:255;index"`
+	RoleNativeID   string      `gorm:"size:255;index"`
 	GrantedDate    time.Time
 	ExpirationDate time.Time
 }
